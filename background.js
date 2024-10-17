032401291f1bea970431991c1895349a26e876a2
Oleksandr Maslakov <jmas.ukraine@gmail.com>
2024-10-17 20:22:06 +0300

    update ID of context menu

--- background.js
+++ background.js
@@ -3,13 +3,13 @@ const DEFAULT_PROMPT = `Express what you see on the page:`;
 chrome.contextMenus.removeAll();
 
 chrome.contextMenus.create({
-  id: "ujournal-to-gpt",
+  id: "send-to-gpt",
   title: "Send to ChatGPT with Prompt",
   contexts: ["all"],
 });
 
 chrome.contextMenus.onClicked.addListener(async (info, tab) => {
-  if (info.menuItemId == "ujournal-to-gpt") {
+  if (info.menuItemId == "send-to-gpt") {
     const settings = await chrome.storage.sync.get("prompt");
 
     const params = new URLSearchParams({
